cc9303ffbc70310b3e59872c645554b11089acd5
Max Milk1806 <maxlebedev22@yandex.ru>
2025-05-05 21:09:56 +0300

    Corrects CodeStyle mistakes

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,3 @@
-import file.FileBackedTaskManager;
-import file.Managers;
-import file.TaskManager;
-import task.Epic;
-import task.Subtask;
-import task.Task;
-
-import java.io.File;
-import java.nio.file.Paths;
-
 public class Main {
 
     public static void main(String[] args) {
